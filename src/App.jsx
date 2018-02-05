3bef2b56c7d52b82ae6f6e4e8eb89846b72a516e
guashi_ <1577886278@qq.com>
2018-02-06 01:07:16 +0800

    index css

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,47 @@
+// 导入 react
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter, Route, Link } from "react-router-dom";
 
-// 导入根组件
-import Home from "./components/Home.jsx";
-import About from "./components/About.jsx";
-import Movie from "./components/Movie.jsx";
+// 导入路由模块
+import { HashRouter, Route, Link } from 'react-router-dom';
+
+// 导入 ant 框架
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+// 导入样式
+import styles from './css/app.scss';
+console.log(styles)
 
 export default class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {};
     }
 
     render() {
-        // 使用 HashRouter 把 App 根组件元素包裹起来后，表示网站已经启动路由
-        // 一个网站中，只需使用一次 HashRouter 即可
         return <HashRouter>
-            {/* 在一个 HashRouter 中只能有一个根元素*/}
-            <div>
-                <h1>网站项目根目录</h1>
-                <hr/>
-                <Link to="/home">首页</Link> &nbsp;&nbsp;
-                <Link to="/movie/hot/110">电影</Link> &nbsp;&nbsp;
-                <Link to="/about">关于</Link>
-                <hr/>
-                {/* 
-                    Router 创建的标签，就是路由的规则，
-                        path：表示要匹配的路由
-                        component 表示当前要展示的子组件
-                    Rouer 的作用
-                        1、表示一个路由规则
-                        2、当作占位符，将来匹配到的组件要放到现在的位置
-                */}
-                <Route path="/home" component={ Home }></Route>
-                {/* 
-                    默认情况下，路由中的规则是模糊匹配的。如果匹配成功，就会展示这个路由对应俄组件
-                    如果要匹配参数，可以在匹配规则中，使用 : 修饰符，表示这个位置匹配到的是参数
-                    如果想让路由规则精确匹配，需要为为 route 添加 exact 属性，表示启动精确匹配
-                */}
-                <Route path="/movie/:name/:num" component={ Movie } exact></Route>
-                <Route path="/about" component={ About }></Route>
-            </div>
-            </HashRouter>
+            <Layout className="layout" style={{ height: '100%' }}>
+            <Header>
+                <div className={ styles.logo } />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['1']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="1">影院首页</Menu.Item>
+                    <Menu.Item key="2">热播电影</Menu.Item>
+                    <Menu.Item key="3">关于我们</Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px', background: '#fff' }}>
+                
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2018 Created by 一杆梅子酒
+            </Footer>
+        </Layout>
+        </HashRouter>
     }
 }
