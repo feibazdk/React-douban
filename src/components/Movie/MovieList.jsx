14d82de7d3ca97027adcfa43f297614a6ecda31e
110 <110@qq.com>
2018-02-06 21:54:20 +0800

    loading

--- src/components/Movie/MovieList.jsx
+++ src/components/Movie/MovieList.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 
+// 导入 loading 组件
+import { Spin, Alert } from 'antd';
+
 
 export default class MovieList extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            movieslist: [],  // 电影列表
+            nowPage: parseInt(props.match.params.page) || 1,  // 当前展示第几页的数据
+            count: 14,  // 每页显示多少条数据
+            total: 0, // 当前电影分类下，总共有多少条数据
+            isloading: true // 数据是否正在加载中
+        }
+    }
+
+    componentWillMount() {
+        setTimeout(() => {
+            this.setState({
+                isloading: false
+            })
+        },1000)
     }
 
     render() {
         return <div>
-            <h1>conent --- { this.props.match.params.type }</h1>
+            { this.renderList() }
         </div>
+    }
+
+    renderList = () => {
+        if(this.state.isloading){
+            return <Spin tip="Loading...">
+                <Alert
+                    message="请稍等……"
+                    description="您查询的页面正在赶来的路上"
+                    type="info"
+                />
+            </Spin>
+        }else{
+            return <h1>数据已经加载完成</h1>
+        }
     }
 }
